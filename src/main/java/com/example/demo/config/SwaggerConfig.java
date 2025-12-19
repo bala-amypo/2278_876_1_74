1c6626fa86ea10da81eb4485e14a22e0b9be651e
bala-amypo <bala@amypo.in>
2025-12-19 09:32:06 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,17 +1,17 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Swagger;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class SwaggerApiConfig {
+public class SwaggerConfig {
 
     @Bean
-    public API customOpenAPI() {
-        return new SwaggerAPI()
+    public Swagger customSwagger() {
+        return new Swagger()
                 // You need to change the port as per your server
                 .servers(List.of(
                         new Server().url("http://9005.vs.amypo.ai")
