a0c57aaab8954e99c201a36006d5e928b8a0c162
bala-amypo <bala@amypo.in>
2025-12-26 05:15:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -6,8 +6,6 @@ import com.example.demo.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -32,7 +30,8 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
     }
 }
