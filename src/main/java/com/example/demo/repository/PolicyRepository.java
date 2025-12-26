5d0486f035538646830d0679451f4143acf60067
bala-amypo <bala@amypo.in>
2025-12-26 05:21:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/PolicyRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.Policy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PolicyRepository extends JpaRepository<Policy, Long> {
@@ -10,4 +11,6 @@ public interface PolicyRepository extends JpaRepository<Policy, Long> {
     Optional<Policy> findByPolicyNumber(String policyNumber);
 
     boolean existsByPolicyNumber(String policyNumber);
+
+    List<Policy> findByUserId(Long userId);
 }
