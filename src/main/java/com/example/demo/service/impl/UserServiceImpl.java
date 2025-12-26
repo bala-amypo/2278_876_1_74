f38f50b228936b01fcef3d4bef94062ea1a05a7b
bala-amypo <bala@amypo.in>
2025-12-26 09:52:50 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -21,11 +21,11 @@ public class UserServiceImpl implements UserService {
     public User register(User user) {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new IllegalArgumentException("Email already exists");
-    }
+        }
 
-    user.setPassword(passwordEncoder.encode(user.getPassword()));
-    return userRepository.save(user);
-}
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 
 
     @Override
